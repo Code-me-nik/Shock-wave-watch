34a23bcf2626f55d99644e9f94fc77a4fafab022
more nikhil <Code-me-nik>
2024-02-23 18:34:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// for minute , second and hour niddle 
+
+let hou = document.getElementById("hour");
+ let min = document.getElementById("minute");
+ let sec = document.getElementById("second");
+
+setInterval( function(){
+    const time = new Date();
+    hou.style.transform = `rotate(${(time.getHours()) * 30}deg)`;
+    min.style.transform = `rotate(${(time.getMinutes() / 60) * 360}deg)`;
+    sec.style.transform = `rotate(${(time.getSeconds() / 60) * 360}deg)`;
+  },1000);
+
+
+  let sprk = document.querySelectorAll(".sparks");
+  
+  setInterval(function () {
+    
+    let value = Math.round(Math.random()*1);
+
+    // console.log( value )
+
+    // sprk[ value ].style.opacity = 1 ;
+    // sprk[ value ].animate = 'flash ease-out 5s infinite'
+     
+  
+}, 1000)
